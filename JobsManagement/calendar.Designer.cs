a2bcb963b3a82ef3982fe72d85e0636bab3b4ead
Tri Dinh Vu <dinhtri205@gmail.com>
2023-04-08 00:14:16 +0700

    update

--- JobsManagement/calendar.Designer.cs
+++ JobsManagement/calendar.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace JobsManagement
 {
-    partial class calendar
+    partial class fcalendar
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(229, 110);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "buttontest";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // fcalendar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "fcalendar";
             this.Text = "calendar";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
