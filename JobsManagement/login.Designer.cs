a8c8565c82c20dc3ebb25a5a17e743f63a22bf9c
dinhtam <114713564+tam010903@users.noreply.github.com>
2023-04-08 01:07:01 +0700

    Tam Update

--- JobsManagement/login.Designer.cs
+++ JobsManagement/login.Designer.cs
@@ -33,7 +33,7 @@
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(241, 91);
+			this.button1.Location = new System.Drawing.Point(209, 293);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(251, 96);
 			this.button1.TabIndex = 0;
