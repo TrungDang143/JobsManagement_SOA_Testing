d4a75fae2abb30f6080d2ba0b2a8b7e2ca23dbb5
unknown <vuduyk94@gmail.com>
2023-04-24 21:54:18 +0700

    test ssh

--- JobsManagement/thongke.Designer.cs
+++ JobsManagement/thongke.Designer.cs
@@ -29,22 +29,23 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.lbCVdaHoanThanh = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lbCVchuaHoanThanh = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
             this.btnStatistics = new System.Windows.Forms.Button();
             this.btnTodayJob = new System.Windows.Forms.Button();
             this.btnCalendar = new System.Windows.Forms.Button();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.panel4 = new System.Windows.Forms.Panel();
-            this.lbCVchuaHoanThanh = new System.Windows.Forms.Label();
-            this.lbCVdaHoanThanh = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
-            this.panel3.SuspendLayout();
             this.panel4.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -57,6 +58,70 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1056, 627);
             this.panel1.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Gainsboro;
+            this.panel4.Controls.Add(this.label1);
+            this.panel4.Controls.Add(this.dataGridView2);
+            this.panel4.Controls.Add(this.lbCVdaHoanThanh);
+            this.panel4.Location = new System.Drawing.Point(536, 79);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(520, 545);
+            this.panel4.TabIndex = 1;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.BackgroundColor = System.Drawing.Color.Gainsboro;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(0, 70);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(520, 475);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // lbCVdaHoanThanh
+            // 
+            this.lbCVdaHoanThanh.AutoSize = true;
+            this.lbCVdaHoanThanh.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lbCVdaHoanThanh.Location = new System.Drawing.Point(3, 23);
+            this.lbCVdaHoanThanh.Name = "lbCVdaHoanThanh";
+            this.lbCVdaHoanThanh.Size = new System.Drawing.Size(379, 29);
+            this.lbCVdaHoanThanh.TabIndex = 0;
+            this.lbCVdaHoanThanh.Text = "Không có công việc đã hoàn thành";
+            this.lbCVdaHoanThanh.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Gainsboro;
+            this.panel3.Controls.Add(this.dataGridView1);
+            this.panel3.Controls.Add(this.lbCVchuaHoanThanh);
+            this.panel3.Location = new System.Drawing.Point(5, 79);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(520, 545);
+            this.panel3.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Gainsboro;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 70);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(520, 475);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // lbCVchuaHoanThanh
+            // 
+            this.lbCVchuaHoanThanh.AutoSize = true;
+            this.lbCVchuaHoanThanh.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lbCVchuaHoanThanh.Location = new System.Drawing.Point(3, 23);
+            this.lbCVchuaHoanThanh.Name = "lbCVchuaHoanThanh";
+            this.lbCVchuaHoanThanh.Size = new System.Drawing.Size(403, 29);
+            this.lbCVchuaHoanThanh.TabIndex = 0;
+            this.lbCVchuaHoanThanh.Text = "Không có công việc chưa hoàn thành";
             // 
             // panel2
             // 
@@ -102,68 +167,15 @@
             this.btnCalendar.Text = "Lịch";
             this.btnCalendar.UseVisualStyleBackColor = true;
             // 
-            // panel3
+            // label1
             // 
-            this.panel3.BackColor = System.Drawing.Color.Gainsboro;
-            this.panel3.Controls.Add(this.dataGridView1);
-            this.panel3.Controls.Add(this.lbCVchuaHoanThanh);
-            this.panel3.Location = new System.Drawing.Point(5, 79);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(520, 545);
-            this.panel3.TabIndex = 1;
-            // 
-            // panel4
-            // 
-            this.panel4.BackColor = System.Drawing.Color.Gainsboro;
-            this.panel4.Controls.Add(this.dataGridView2);
-            this.panel4.Controls.Add(this.lbCVdaHoanThanh);
-            this.panel4.Location = new System.Drawing.Point(536, 79);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(520, 545);
-            this.panel4.TabIndex = 1;
-            // 
-            // lbCVchuaHoanThanh
-            // 
-            this.lbCVchuaHoanThanh.AutoSize = true;
-            this.lbCVchuaHoanThanh.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.lbCVchuaHoanThanh.Location = new System.Drawing.Point(3, 23);
-            this.lbCVchuaHoanThanh.Name = "lbCVchuaHoanThanh";
-            this.lbCVchuaHoanThanh.Size = new System.Drawing.Size(403, 29);
-            this.lbCVchuaHoanThanh.TabIndex = 0;
-            this.lbCVchuaHoanThanh.Text = "Không có công việc chưa hoàn thành";
-            // 
-            // lbCVdaHoanThanh
-            // 
-            this.lbCVdaHoanThanh.AutoSize = true;
-            this.lbCVdaHoanThanh.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.lbCVdaHoanThanh.Location = new System.Drawing.Point(3, 23);
-            this.lbCVdaHoanThanh.Name = "lbCVdaHoanThanh";
-            this.lbCVdaHoanThanh.Size = new System.Drawing.Size(379, 29);
-            this.lbCVdaHoanThanh.TabIndex = 0;
-            this.lbCVdaHoanThanh.Text = "Không có công việc đã hoàn thành";
-            this.lbCVdaHoanThanh.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.BackgroundColor = System.Drawing.Color.Gainsboro;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 70);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(520, 475);
-            this.dataGridView1.TabIndex = 1;
-            // 
-            // dataGridView2
-            // 
-            this.dataGridView2.BackgroundColor = System.Drawing.Color.Gainsboro;
-            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(0, 70);
-            this.dataGridView2.Name = "dataGridView2";
-            this.dataGridView2.RowHeadersWidth = 51;
-            this.dataGridView2.RowTemplate.Height = 24;
-            this.dataGridView2.Size = new System.Drawing.Size(520, 475);
-            this.dataGridView2.TabIndex = 1;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(76, 151);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Khanh";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // fThongKe
             // 
@@ -174,13 +186,13 @@
             this.Name = "fThongKe";
             this.Text = "thongke";
             this.panel1.ResumeLayout(false);
-            this.panel2.ResumeLayout(false);
-            this.panel3.ResumeLayout(false);
-            this.panel3.PerformLayout();
             this.panel4.ResumeLayout(false);
             this.panel4.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -198,5 +210,6 @@
         private System.Windows.Forms.Label lbCVchuaHoanThanh;
         private System.Windows.Forms.DataGridView dataGridView2;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
     }
 }
