00bc145c1fda232b49ca44150abe6f6215eb0f65
Tri Dinh Vu <dinhtri205@gmail.com>
2023-05-23 14:04:18 +0700

    update

--- JobsManagement/fForgot.Designer.cs
+++ JobsManagement/fForgot.Designer.cs
@@ -37,74 +37,90 @@
             this.lbEmail = new System.Windows.Forms.Label();
             this.lbMK = new System.Windows.Forms.Label();
             this.lbCopied = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.icoMinus = new System.Windows.Forms.PictureBox();
+            this.icoClose = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.icoMinus)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.icoClose)).BeginInit();
             this.SuspendLayout();
             // 
             // btnCancel
             // 
-            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnCancel.BackColor = System.Drawing.Color.Turquoise;
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.btnCancel.Location = new System.Drawing.Point(328, 270);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(130, 50);
             this.btnCancel.TabIndex = 3;
             this.btnCancel.Text = "Huỷ";
-            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.UseVisualStyleBackColor = false;
             // 
             // btnLayLaiMK
             // 
-            this.btnLayLaiMK.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.btnLayLaiMK.Location = new System.Drawing.Point(70, 270);
+            this.btnLayLaiMK.BackColor = System.Drawing.Color.Turquoise;
+            this.btnLayLaiMK.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLayLaiMK.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLayLaiMK.Location = new System.Drawing.Point(69, 270);
             this.btnLayLaiMK.Name = "btnLayLaiMK";
             this.btnLayLaiMK.Size = new System.Drawing.Size(130, 50);
             this.btnLayLaiMK.TabIndex = 2;
-            this.btnLayLaiMK.Text = "Lấy lại";
-            this.btnLayLaiMK.UseVisualStyleBackColor = true;
+            this.btnLayLaiMK.Text = "Xác nhận";
+            this.btnLayLaiMK.UseVisualStyleBackColor = false;
             // 
             // textBox4
             // 
+            this.textBox4.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.textBox4.Location = new System.Drawing.Point(145, 111);
+            this.textBox4.Location = new System.Drawing.Point(145, 135);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(313, 30);
+            this.textBox4.Size = new System.Drawing.Size(313, 23);
             this.textBox4.TabIndex = 1;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label5.Location = new System.Drawing.Point(65, 116);
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Turquoise;
+            this.label5.Location = new System.Drawing.Point(65, 135);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(66, 25);
+            this.label5.Size = new System.Drawing.Size(67, 23);
             this.label5.TabIndex = 3;
             this.label5.Text = "Email:";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label1.Location = new System.Drawing.Point(138, 32);
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Turquoise;
+            this.label1.Location = new System.Drawing.Point(138, 45);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(264, 39);
+            this.label1.Size = new System.Drawing.Size(258, 40);
             this.label1.TabIndex = 7;
             this.label1.Text = "Lấy lại mật khẩu";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label2.Location = new System.Drawing.Point(65, 182);
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Turquoise;
+            this.label2.Location = new System.Drawing.Point(65, 205);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(179, 25);
+            this.label2.Size = new System.Drawing.Size(186, 23);
             this.label2.TabIndex = 3;
             this.label2.Text = "Mật khẩu của bạn: ";
             // 
             // lbEmail
             // 
             this.lbEmail.AutoSize = true;
-            this.lbEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lbEmail.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbEmail.ForeColor = System.Drawing.Color.Red;
-            this.lbEmail.Location = new System.Drawing.Point(141, 144);
+            this.lbEmail.Location = new System.Drawing.Point(141, 161);
             this.lbEmail.Name = "lbEmail";
-            this.lbEmail.Size = new System.Drawing.Size(156, 20);
+            this.lbEmail.Size = new System.Drawing.Size(173, 21);
             this.lbEmail.TabIndex = 14;
             this.lbEmail.Text = "Email không tồn tại!";
             this.lbEmail.Visible = false;
@@ -112,11 +128,11 @@
             // lbMK
             // 
             this.lbMK.AutoSize = true;
-            this.lbMK.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.lbMK.ForeColor = System.Drawing.Color.Red;
-            this.lbMK.Location = new System.Drawing.Point(250, 182);
+            this.lbMK.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMK.ForeColor = System.Drawing.Color.White;
+            this.lbMK.Location = new System.Drawing.Point(249, 205);
             this.lbMK.Name = "lbMK";
-            this.lbMK.Size = new System.Drawing.Size(98, 25);
+            this.lbMK.Size = new System.Drawing.Size(103, 23);
             this.lbMK.TabIndex = 15;
             this.lbMK.Text = "Password";
             this.lbMK.Visible = false;
@@ -124,20 +140,62 @@
             // lbCopied
             // 
             this.lbCopied.AutoSize = true;
-            this.lbCopied.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.lbCopied.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.lbCopied.Location = new System.Drawing.Point(250, 217);
+            this.lbCopied.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCopied.ForeColor = System.Drawing.Color.Aqua;
+            this.lbCopied.Location = new System.Drawing.Point(250, 230);
             this.lbCopied.Name = "lbCopied";
-            this.lbCopied.Size = new System.Drawing.Size(75, 20);
+            this.lbCopied.Size = new System.Drawing.Size(102, 23);
             this.lbCopied.TabIndex = 15;
             this.lbCopied.Text = "Đã copy!";
             this.lbCopied.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(141, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(269, 21);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Vui lòng điền thông tin dưới đây";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.icoClose);
+            this.panel1.Controls.Add(this.icoMinus);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(531, 33);
+            this.panel1.TabIndex = 17;
+            // 
+            // icoMinus
+            // 
+            this.icoMinus.Image = global::JobsManagement.Properties.Resources.minus_icon;
+            this.icoMinus.Location = new System.Drawing.Point(462, 1);
+            this.icoMinus.Name = "icoMinus";
+            this.icoMinus.Size = new System.Drawing.Size(30, 30);
+            this.icoMinus.TabIndex = 18;
+            this.icoMinus.TabStop = false;
+            // 
+            // icoClose
+            // 
+            this.icoClose.Image = global::JobsManagement.Properties.Resources.multipy_icon;
+            this.icoClose.Location = new System.Drawing.Point(498, 0);
+            this.icoClose.Name = "icoClose";
+            this.icoClose.Size = new System.Drawing.Size(30, 30);
+            this.icoClose.TabIndex = 19;
+            this.icoClose.TabStop = false;
             // 
             // fForgot
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
             this.ClientSize = new System.Drawing.Size(531, 355);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.lbCopied);
             this.Controls.Add(this.lbMK);
             this.Controls.Add(this.lbEmail);
@@ -147,8 +205,12 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "fForgot";
             this.Text = "fForgot";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.icoMinus)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.icoClose)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -165,5 +227,9 @@
         private System.Windows.Forms.Label lbEmail;
         private System.Windows.Forms.Label lbMK;
         private System.Windows.Forms.Label lbCopied;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox icoMinus;
+        private System.Windows.Forms.PictureBox icoClose;
     }
 }
--- JobsManagement/fForgot.cs
+++ JobsManagement/fForgot.cs
@@ -16,5 +16,6 @@ namespace JobsManagement
         {
             InitializeComponent();
         }
+
     }
 }
